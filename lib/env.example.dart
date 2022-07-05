fe3ce5e5b5fc47a9ff32e69d7bc94b16d8f6aeb5
Weliton Sousa <64853142+welitonsousa@users.noreply.github.com>
2022-07-05 12:24:09 -0300

    update env example

--- lib/env.example.dart
+++ lib/env.example.dart
@@ -3,7 +3,10 @@ class Env {
   static const TIME_OUT_MILLISECONDS = 30000;
   static const DEBUG_MODE = false;
   static const BASE_URL = 'https://api.themoviedb.org/3';
-  static const API_KEY = 'your api key';
+  static const API_KEY = 'your key';
   static const IMAGE_BASE = 'https://image.tmdb.org/t/p/w300/';
+  static const IMAGE_AVATAR =
+      'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=612x612&w=0&h=lGpj2vWAI3WUT1JeJWm1PRoHT3V15_1pdcTn2szdwQ0=';
 }
+
 */
